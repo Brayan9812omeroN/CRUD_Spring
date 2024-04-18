0b9206fe9536a27be33c281740aff1c248f2de52
Brayan9812omeroN <142071616+Brayan9812omeroN@users.noreply.github.com>
2024-04-17 23:22:11 -0500

    CRUD SpringJava

---
+++ src/main/java/com/utadeo/taskapp/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.utadeo.taskapp.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.utadeo.taskapp.model.User;
+import com.utadeo.taskapp.service.IUserService;
+
+
+@RestController
+public class UserController {
+
+    private IUserService userService;
+
+    UserController(IUserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("user/")
+    public User store(@RequestBody User user){
+        return this.userService.createUser(user);
+    }
+    
+    
+}
