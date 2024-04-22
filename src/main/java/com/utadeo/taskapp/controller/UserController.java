e9ffd677ee39bc4b8745289583cd09d88a3c14d3
Brayan9812omeroN <142071616+Brayan9812omeroN@users.noreply.github.com>
2024-04-21 20:24:40 -0500

    Finsish CRUD

--- src/main/java/com/utadeo/taskapp/controller/UserController.java
+++ src/main/java/com/utadeo/taskapp/controller/UserController.java
@@ -1,6 +1,12 @@
 package com.utadeo.taskapp.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.utadeo.taskapp.model.User;
@@ -20,6 +26,26 @@ public class UserController {
     public User store(@RequestBody User user){
         return this.userService.createUser(user);
     }
+    
+    @PutMapping("/user/{id}")
+	public User updateUser(@PathVariable Long id ,@RequestBody User user) {
+		return userService.updateUser(id,user);
+	}
+    
+    @GetMapping("/{id}/")
+    public User show(@PathVariable("id") Long id){
+        return this.userService.getById(id);
+    }
+
+    @GetMapping("allusers/")
+    public List<User> AllUsers(){
+		return userService.getAllUsers();
+	}
+
+    @DeleteMapping("/user/{id}/")
+	public void deleteUser(@PathVariable("id") Long id) {
+		this.userService.deleteUser(id);
+	}
     
     
 }
